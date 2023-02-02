b84b2135479523985ef93b1601e2a77b46d480bb
Svetlana Kutyova <kutyovas@yandex.ru>
2023-02-03 00:00:13 +0300

    ticket!

--- app/Http/Controllers/SeatController.php
+++ app/Http/Controllers/SeatController.php
@@ -74,25 +74,18 @@ class SeatController extends Controller
         $seatnull= ($seance == null) ? null : Seat::all()->where('seance_id', $seance['id'])->where('hall_id', $hall['id']);
         $seats = $request['seats'] ?? $seatnull;
         $selected = $request['selected'] ?? [];
-        //dump($selected);//dump(json_decode($selected));//dump(json_decode($selected)[0]);//dump(count(json_decode($selected)));
-        //dump(explode(',',  json_decode($selected)[0]));//dump((int)explode(',',  json_decode($selected)[0])[0]);
-
         $seatts =[];
         for ($i = 0, $iMax = count(json_decode($selected)); $i < $iMax; $i ++) {
             $seatts[]= Seat::all()->where('seance_id', $seance['id'])->where('hall_id', $hall['id'])->where('rowNumber', (int) explode(',',  json_decode($selected)[$i])[0])->where('colNumber', (int) explode(',', json_decode($selected)[$i])[1])->first();
-            //dump($seatts);dump($seatts[$i]);
         }
-       // dump('сколько выбрано мест'.count($seatts));//
+
         $ticket= count(Ticket::all());
         $count_ticket = 0;
 
         for ($i = 0, $iMax = count($seatts); $i < $iMax; $i ++) {
-            //var_dump($seatts[$i]);dump($seatts[$i]);//var_dump($seatts[$i]['free']);dump($seatts[$i]['ticket_id']);
-            $seatts[$i]["free"]= "0";
+           $seatts[$i]["free"]= "0";
             $seatts[$i]["ticket_id"]= (string) ($ticket+1);
-            $ticket++;
             $ij = $seatts[$i]['rowNumber'].','.$seatts[$i]['colNumber'];
-            //dd($ij);
             if($hall_decode[$ij]==='NORM') {
                 $count_ticket += $hall['countNormal'];
             }
@@ -102,7 +95,6 @@ class SeatController extends Controller
 
             $seatts[$i]->save();//$this->update($seatts[$i]);
         }
-        //dump('изменили');//dd($seatts);dd($count_ticket);//return redirect()->route('client.ticket',['count'=> $count_ticket,'selected'=> $selected, 'film' => $film, 'hall' => $hall, 'seance'=> $seance, 'dateChosen'=> $dateChosen, 'seats'=> $seats]);
         return redirect()->route('client.ticket.create',['count'=> $count_ticket,'selected'=> $selected, 'film' => $film, 'hall' => $hall, 'seance'=> $seance, 'dateChosen'=> $dateChosen, 'seats'=> $seats]);
     }
 
@@ -115,8 +107,7 @@ class SeatController extends Controller
      */
     public function update(Request $request, Seat $seat)
     {
-        //$seat->fill($request->validated());
-        //return $seat->save();
+        //
     }
 
     /**
